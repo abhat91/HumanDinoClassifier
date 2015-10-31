b203bcd3c7b3ca8f2e97a99c2d5af8de75b44707
Aditya <abhat@wpi.edu>
2015-10-31 11:59:31 -0400

    Added method comments

--- src/dinoclassifier.py
+++ src/dinoclassifier.py
@@ -1,12 +1,14 @@
 import cv2
 import utils
 
-
+#Displays the image given the frame name and the frame object
 def showImage(frameName, frame):
+    """Displays the image given the frame name and the frame object: showImage(frameName, frame)"""
     cv2.imshow(frameName,frame)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+#The main code which has to be extended
 dinoImage=utils.Util.getCurrentFolder()+"/../"+"testimages/dino3.png"
 frame = cv2.imread(dinoImage,0)
 edges = cv2.Canny(frame,150,200)
