fca6eeccfb2d1bfb014f0110b4f075c8e4258605
Renato Girardi Gasoto <rggasoto@wpi.edu>
2015-12-05 22:54:08 -0500

    dd convex hull

--- src/dinoclassifier.py
+++ src/dinoclassifier.py
@@ -1,13 +1,31 @@
 import cv2
 import utils
+import copy
 
 frameName = 'Canny Output'
 def updateCanny(arg):
     lt = cv2.getTrackbarPos('Edge Linking',frameName)
     ht = cv2.getTrackbarPos('Strong Edges',frameName)
     edges = cv2.Canny(frame,lt,ht)
+    frame2 = copy.deepcopy(frame)
+    contours, _ = cv2.findContours(edges,1,2)
+    for cnt in contours:
+        hull = cv2.convexHull(cnt,returnPoints = False)
+        if(len(hull)>3 and len(cnt)>3):
+
+            defects = cv2.convexityDefects(cnt,hull)
+            if defects!=None:
+                for i in range(defects.shape[0]):
+                    s,e,f,d = defects[i,0]
+                    start = tuple(cnt[s][0])
+                    end = tuple(cnt[e][0])
+                    far = tuple(cnt[f][0])
+                    cv2.line(frame2,start,end,[0,255,0],2)
+                    cv2.circle(frame2,far,5,[0,0,255],-1)
+
     edges = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
-    frame2 = cv2.add(frame,edges)
+    frame2 = cv2.add(frame2,edges)
+
     cv2.imshow(frameName,frame2)
 #Displays the image given the frame name and the frame object
 def showImage(frameName, frame):
@@ -23,13 +41,13 @@ def showImage(frameName, frame):
     cv2.destroyAllWindows()
 
 #The main code which has to be extended
-dinoImage=utils.Util.getCurrentFolder()+"/../"+"testimages/dino2.png"
+dinoImage=utils.Util.getCurrentFolder()+"/../"+"testimages/dino5.png"
 frame = cv2.imread(dinoImage)
 frame = cv2.bilateralFilter(frame,9,75,75)
 edges = cv2.Canny(frame,0,0)
+# frame2 = copy.deepcopy(frame)
+# cv2.drawContours(frame2,countours,0,(0,255,0),2)
 edges = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
 frame2 = cv2.add(frame,edges)
 
 showImage(frameName, frame2)
-
-
